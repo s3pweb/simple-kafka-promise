e13d4d17f80e4dc0ddf3ed2fadd69b3eaa5ddb77
Guillaume GABOREL <guillaume.gaborel@s3pweb.com>
2019-04-24 14:30:40 +0200

    fix(HLProducer): added missing export

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 module.exports.producer = require('./lib/kafkaNproducer')
+module.exports.highLevelProducer = require('./lib/highLevel/kafkaHighLevelProducer')
 module.exports.consumer = require('./lib/kafkaNconsumer')
