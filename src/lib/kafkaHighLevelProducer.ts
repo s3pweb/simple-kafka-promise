16878b72fe694985e5724a02de2aca807f131dda
Guillaume GABOREL <guillaume.gaborel@s3pweb.com>
2020-01-13 15:34:24 +0100

    feat(producer): removed node-config dependency in producer

--- src/lib/kafkaHighLevelProducer.ts
+++ src/lib/kafkaHighLevelProducer.ts
@@ -1,16 +1,21 @@
-import * as config from 'config';
 import { HighLevelProducer } from 'node-rdkafka';
 
 export class KafkaProducer {
   private connected: boolean;
-  private readonly producerConfig: any;
+  private readonly prefix: string;
   private readonly producer: HighLevelProducer;
 
-  constructor() {
+  /**
+   *
+   * @param config Node-rdkafka configuration object. Minimum: `{ "metadata.broker.list": "0.0.0.0:9094" }`
+   * @param topicPrefix Prefix to add before each topic name
+   */
+  constructor(config: object, topicPrefix?: string) {
     this.connected = false;
+    this.prefix = topicPrefix ? topicPrefix : '';
 
-    this.producerConfig = {
-      ...(config.get('kafka.producer.config')),
+    const producerConfig = {
+      ...config,
       ...{
         'socket.keepalive.enable': true,
         'request.required.acks': 1, // wait for leader ack
@@ -18,11 +23,12 @@ export class KafkaProducer {
       },
     };
 
-    this.producer = new HighLevelProducer(this.producerConfig, {});
+    this.producer = new HighLevelProducer(producerConfig, {});
   }
 
   /**
    * Connect the producer to kafka, will return broker's metadata or nothing if already connected.
+   * @return Broker's metadata
    */
   connect(): Promise<object | null> {
     return new Promise((resolve, reject) => {
@@ -77,17 +83,14 @@ export class KafkaProducer {
    * @param message Message to be sent (will be parsed with `JSON.stringify(...)` before).
    * @param partition Topic partition.
    * @param key Kafka key to be sent along the message.
+   * @return Message's offset
    */
   sendMessage(topic: string, message: object, partition: number, key: any): Promise<number> {
     return new Promise((resolve, reject) => {
-      // Topic prefix
-      let prefix = '';
-      // Check if prefix exist before accessing it
-      if (config.has('kafka.producer.topicsPrefix')) {
-        prefix = config.get('kafka.producer.topicsPrefix');
-      }
-      const fullTopic = prefix + topic;
+      // Create full topic
+      const fullTopic = this.prefix + topic;
 
+      // Send message to kafka
       this.producer.produce(
         fullTopic,
         partition,
